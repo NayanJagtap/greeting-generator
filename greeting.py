ffcfe5ba98ae42b0019c8a79af5b776e1960dcdc
Nayan2 <nayandinkarjagtap@gmail.com>
2025-07-06 07:12:21 +0000

    added time based greeting

--- greeting.py
+++ greeting.py
@@ -1,6 +1,15 @@
+import datetime
+
 def greet(name):
-    return f"hello,{name}!"
+    current_hour=datetime.datetime.now().hour
+    if 5 <=current_hour <12:
+        greeting = "Good Morning "
+    elif 12<=current_hour<18:
+        greeting = "Good Afternoon"
+    else:
+        greeting = "Good Evening"
+    return f"{greeting},{name}!"
 
 if __name__ == "__main__":
-    print(greet("World"))
+    print(greet("Friend"))
 
